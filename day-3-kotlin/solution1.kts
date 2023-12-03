678ecebd104cc41190255940090bd24ed47daaac
Stephen Choo <stephen.w.choo@gmail.com>
2023-12-03 17:56:48 +1100

    Day 3 done, I've written some absolutely disgusting Kotlin - but it works.
    The lack of a proper Kotlin IDE is really hurting me - might see if I can somehow jury rig up Android Studio as a proper IDE

--- day-3-kotlin/solution1.kts
+++ day-3-kotlin/solution1.kts
@@ -1,7 +1,101 @@
 import java.io.File
 
+fun generateNumberPositions(schematic: List<List<String>>): HashMap<List<Pair<Int, Int>>, Int> {
+    val numbers = HashMap<List<Pair<Int, Int>>, Int>()
+    val H = schematic.size
+    val W = schematic[0].size
+
+    for (i in 0 until H) {
+        var currentNum = ""
+        var currentCoOrds: MutableList<Pair<Int, Int>> = mutableListOf()
+
+        for (j in 0 until W) {
+            val char = schematic[i][j]
+            if (!char.first().isDigit() && currentNum.isNotEmpty()) {
+                numbers.put(currentCoOrds, currentNum.toInt())
+                currentNum = ""
+                currentCoOrds = mutableListOf()
+            }
+            if (char.isNotEmpty() && char.first().isDigit()) {
+                currentNum += char
+                currentCoOrds.add(Pair(i, j))
+            }
+        }
+        if (currentNum.isNotEmpty()) {
+            numbers.put(currentCoOrds, currentNum.toInt())
+        }
+    }
+    return numbers
+}
+
+fun checkAdjacents(position: Pair<Int, Int>, symbols: Set<Pair<Int,Int>>): Boolean {
+    val adjacents = listOf(
+        Pair(-1, -1),
+        Pair(-1, 0),
+        Pair(-1, 1),
+        Pair(0, -1),
+        Pair(0, 1),
+        Pair(1, -1),
+        Pair(1, 0),
+        Pair(1, 1)
+    )
+
+    for (adjacent in adjacents) {
+        val newPosition = Pair(position.first + adjacent.first, position.second + adjacent.second)
+        if (symbols.contains(newPosition)) {
+            return true
+        }
+    }
+    return false
+}
+
+
 fun solution(input: String): String {
-    // Your solution here
+    // Make a 2d array of characters
+    var res = 0
+
+    val schematic: List<List<String>> = input.split("\n").map { line -> line.split("").filter { it.isNotEmpty() } }
+    val H = schematic.size
+    val W = schematic[0].size
+
+    // Make a set of all the number positions
+    val numbers = mutableSetOf<Pair<Int, Int>>()
+    schematic.forEachIndexed { i, line ->
+        line.forEachIndexed { j, char ->
+            if (char.first() != '.' && !char.first().isDigit()) {
+                symbols.add(Pair(i, j))
+            }
+        }
+    }
+
+    // Make a set of the symbol positions we've already visited
+    val symbols = mutableSetOf<Pair<Int, Int>>()
+
+    schematic.forEachIndexed { i, line ->
+        line.forEachIndexed { j, char ->
+            if (char.first() != '.' && !char.first().isDigit()) {
+                symbols.add(Pair(i, j))
+            }
+        }
+    }
+
+    // Iterate through the hashmap and add any numbers to res
+    for ((positions, number) in numbers) {
+        var numberAdded = false
+
+        for (position in positions) {
+            if (numberAdded) {
+                continue
+            }
+            if (checkAdjacents(position, symbols)) {
+                res += number
+                numberAdded = true
+                continue
+            }
+        }
+    }
+
+    return res.toString()
 }
 
 fun runSolution(inputFilePath: String, solutionFunction: (String) -> String) {
--- day-3-kotlin/solution2.kts
+++ day-3-kotlin/solution2.kts
@@ -1,7 +1,81 @@
 import java.io.File
 
+fun getNumberAtCoords(schematic: List<List<String>>, coord: Pair<Int, Int>): Int {
+    // Move left until no number is found
+    var y = coord.first
+    var x = coord.second
+
+    while (x > 0 && schematic[y][x - 1].first().isDigit()) {
+        x -= 1
+    }
+
+    var number = ""
+
+    while (x < schematic[0].size && schematic[y][x].first().isDigit()) {
+        number += schematic[y][x].first()
+        x += 1
+    }
+
+    return number.toInt()
+}
+
+
+fun checkAdjacentsForNumbers(position: Pair<Int, Int>, schematic: List<List<String>>): Set<Int> {
+    val adjacents = listOf(
+        Pair(-1, -1),
+        Pair(-1, 0),
+        Pair(-1, 1),
+        Pair(0, -1),
+        Pair(0, 1),
+        Pair(1, -1),
+        Pair(1, 0),
+        Pair(1, 1)
+    )
+
+    val numbers = mutableSetOf<Int>()
+
+    for (adjacent in adjacents) {
+        val newPosition = Pair(position.first + adjacent.first, position.second + adjacent.second)
+        if (schematic[newPosition.first][newPosition.second].first().isDigit()) {
+            numbers.add(getNumberAtCoords(schematic, newPosition))
+        }
+    }
+    return numbers
+}
+
+
 fun solution(input: String): String {
-    // Your solution here
+    // Make a 2d array of characters
+    var res = 0
+
+    val schematic: List<List<String>> = input.split("\n").map { line -> line.split("").filter { it.isNotEmpty() } }
+    val H = schematic.size
+    val W = schematic[0].size
+
+    // Make a set of the gear positions
+    val gearPositions = mutableSetOf<Pair<Int, Int>>()
+
+    schematic.forEachIndexed { i, line ->
+        line.forEachIndexed { j, char ->
+            if (char.first() == '*') {
+                gearPositions.add(Pair(i, j))
+            }
+        }
+    }
+
+    // Iterate through the gears
+    for (gearPosition in gearPositions) {
+        val numbers = mutableSetOf<Int>()
+
+        val gearNumbers = checkAdjacentsForNumbers(gearPosition, schematic).toList()
+        println(gearNumbers)
+        if (gearNumbers.size == 2) {
+            res += gearNumbers[0] * gearNumbers[1]
+        }
+    }
+
+
+    return res.toString()
 }
 
 fun runSolution(inputFilePath: String, solutionFunction: (String) -> String) {
