965837e635d0367f47f8f8b3357aca78dbe1497c
Stephen Choo <stephen.w.choo@gmail.com>
2023-12-05 22:47:23 +1100

    I've verified each step with the test input, and it's successful there - but I cannot work out why it's failing on the actual output

--- day-5-typescript/solution2.ts
+++ day-5-typescript/solution2.ts
@@ -28,50 +28,60 @@ function solution(input: string): string {
         let newRanges: Range[] = []
         
         for (let mapLine of individualMap) {
-            let [destinationStart, sourceStart, rangeLength] = mapLine
+            let [mapDestinationStart, mapRangeStart, mapRangeLength] = mapLine
 
-            let difference = sourceStart - destinationStart
+            let valueTransformer = mapDestinationStart - mapRangeStart
+            let mapRangeEnd = mapRangeStart + mapRangeLength
 
             let rangesToRemove = new Set()
             let rangesToAddBack: Range[] = []
-        
+            
+            // current ranges are the ranges that have not been transformed yet
+            // new ranges are the ranges that have been transformed
             currentRanges.forEach((currentRange) => {
                 // check if the ranges intersect
-                let [rangeStart, rangeExtension] = currentRange
-                let rangeEnd = rangeStart + rangeExtension
+                let [currentRangeStart, currentRangeLength] = currentRange
+                let currentRangeEnd = currentRangeStart + currentRangeLength
 
                 // current Range is within sourceStart -> rangeLength
                 // remove it from the current range
                 // transform the whole range and add to new ranges
-                if (sourceStart <= rangeStart && rangeEnd <= (sourceStart + rangeLength)) {
+                // this step is correct - verified
+                if (mapRangeStart <= currentRangeStart && currentRangeEnd <= mapRangeEnd) {
                     rangesToRemove.add(currentRange)
-                    newRanges.push([rangeStart - difference, rangeExtension])
+                    newRanges.push([currentRangeStart + valueTransformer, currentRangeLength])
                 } 
                 // if rangeStart is within sourceStart -> rangeLength
                 // remove it from the current range
-                // transform the part from rangeStart -> sourceStart + rangeLength and add to newRanges
+                // transform the part from currentRangeStart -> mapRangeEnd and add to newRanges
                 // add back sourceStart + rangeLength -> (sourceStart + rangeLength) - (rangeStart + rangeExtension)
-                else if (sourceStart <= rangeStart && rangeStart <= (sourceStart + rangeLength)) {
+                // this step also seems to be behaving as expected
+                else if (mapRangeStart <= currentRangeStart && currentRangeStart < mapRangeEnd) {
+                    let newRangeLength = mapRangeEnd - currentRangeStart
+
                     rangesToRemove.add(currentRange)
-                    newRanges.push([rangeStart - difference, sourceStart + rangeLength - rangeStart])
-                    rangesToAddBack.push([sourceStart + rangeLength, rangeEnd - (sourceStart + rangeLength)])
+                    newRanges.push([currentRangeStart + valueTransformer, newRangeLength])
+                    rangesToAddBack.push([currentRangeStart + newRangeLength, currentRangeLength - newRangeLength])
                 } 
                 // if rangeEnd is within sourceStart -> rangeLength
                 // remove it from the current range
                 // transform the part from sourceStart -> rangeEnd and add to newRanges
                 // add back rangeEnd -> (sourceStart + rangeLength) - (rangeStart + rangeExtension)
-                else if (rangeEnd <= (sourceStart + rangeLength)) {
+                // this step is also behaving as expected if it gets to this point
+                else if (currentRangeEnd <= mapRangeEnd && mapRangeStart < currentRangeEnd) {
+                    let newRangeLength = currentRangeEnd - mapRangeStart
                     rangesToRemove.add(currentRange)
-                    newRanges.push([sourceStart, rangeEnd - sourceStart])
-                    rangesToAddBack.push([rangeEnd, sourceStart + rangeLength - rangeEnd])
+                    newRanges.push([mapRangeStart + valueTransformer, newRangeLength])
+                    rangesToAddBack.push([currentRangeStart, currentRangeLength - newRangeLength])
                 }
                 
             })
-            // remove the ranges to remove, add back the ranges to add back
+            // update the ranges after each map line to only transform a range once
             currentRanges = currentRanges.filter((number) => !rangesToRemove.has(number))
             currentRanges = [...rangesToAddBack, ...currentRanges]
         }      
         currentRanges = [...newRanges, ...currentRanges]
+        console.log(currentRanges)
     }
     let rangeStarts = currentRanges.map((range) => range[0])
     return Math.min(...rangeStarts).toString()
